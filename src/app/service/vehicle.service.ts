08edd4112eb540e5af2ff42ee72cb49e98d7235c
lauratellez321 <--global>
2024-01-26 02:14:08 -0500

    Implementacion de los demas servicios para obtener la informacion final de vehiculo

--- src/app/service/vehicle.service.ts
+++ src/app/service/vehicle.service.ts
@@ -3,17 +3,67 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
 
+type typeVehicle = 'car' | 'motocycle' | 'truck';
+
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleService {
   constructor(private readonly httpClient: HttpClient) {}
 
-  getVehiculo(vehicle: 'car' | 'motocycle' | 'truck') {
+  getVehicle(vehicle: typeVehicle) {
     const url =
       environment.api.urlBase +
       environment.api.typeVehicle[vehicle] +
       environment.api.brand;
+    return this.httpClient.get(url);
+  }
+  getBrandByVehicle(vehicle: typeVehicle, brand: string) {
+    const url =
+      environment.api.urlBase +
+      environment.api.typeVehicle[vehicle] +
+      environment.api.brand +
+      brand +
+      '/' +
+      environment.api.model;
+
+    return this.httpClient.get(url);
+  }
+
+  getModelByBrand(vehicle: typeVehicle, brand: string, model: string) {
+    const url =
+      environment.api.urlBase +
+      environment.api.typeVehicle[vehicle] +
+      environment.api.brand +
+      brand +
+      '/' +
+      environment.api.model +
+      model +
+      '/' +
+      environment.api.years;
+
+    return this.httpClient.get(url);
+  }
+
+  getValueByModel(
+    vehicle: typeVehicle,
+    brand: string,
+    model: string,
+    year: string
+  ) {
+    const url =
+      environment.api.urlBase +
+      environment.api.typeVehicle[vehicle] +
+      environment.api.brand +
+      brand +
+      '/' +
+      environment.api.model +
+      model +
+      '/' +
+      environment.api.years +
+      year +
+      '/';
+
     return this.httpClient.get(url);
   }
 }
