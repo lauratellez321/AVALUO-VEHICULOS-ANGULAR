9560ff180afafbeb84e6941a85dfcd6124988982
lauratellez321 <--global>
2024-01-25 21:37:15 -0500

    Generacion de Componente y Maquetacion Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehicleComponent } from './core/vehicle/vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
